c5c467434f97f4d014797ad3d923dc9c402bccdc
Sumit Pasupalak <pasupalaks@gmail.com>
2013-07-25 19:40:43 -0400

    HTTP server base code

---
+++ src/httpServer.js
@@ -0,0 +1,11 @@
+var http = require('http)
+
+http.createServer(function(request, response) {
+        var ip = request.connection.remoteAddress;
+        var port = request.connection.port;
+        console.log('Connection received from ' + ip + ' ' + port);
+        response.writeHead(200, {'Content-Type': 'text'});
+        response.write(ip + ' ' + port);
+        response.end();
+}).listen(25000);
+~                 
